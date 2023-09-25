4c2d8e220b206fd89410830f81a9fe8efc9997d2
KrazyyyPotatoo <116055802+KrazyyyPotatoo@users.noreply.github.com>
2023-09-25 21:24:55 +0800

    practice_set1

--- practice_set1.php
+++ practice_set1.php
@@ -1,62 +1,39 @@
 <?php
-/**
- * Calculates the total price of items in a shopping cart.
- *
- * @param array $items An array of items with 'name' and 'price' keys.
- * @return float The total price of the items...
- */
-function calculateTotalPrice(array $items): float {
+function calculateTotalPrice($items)
+{
     $total = 0;
     foreach ($items as $item) {
         $total += $item['price'];
     }
     return $total;
 }
-
-/**
- * Modifies a string by removing spaces and converting it to lowercase.
- *
- * @param string $inputString The input string to be modified.
- * @return string The modified string.
- */
-function modifyString(string $inputString): string {
-    $modifiedString = str_replace(' ', '', $inputString);
-    $modifiedString = strtolower($modifiedString);
-    return $modifiedString;
-}
-
-/**
- * Checks if a number is even or odd.
- *
- * @param int $number The number to be checked.
- * @return string A message indicating whether the number is even or odd.
- */
-function checkEvenOrOdd(int $number): string {
-    if ($number % 2 == 0) {
-        return "The number $number is even.";
-    } else {
-        return "The number $number is odd.";
-    }
-}
-
-// Shopping cart items.
 $items = [
     ['name' => 'Widget A', 'price' => 10],
     ['name' => 'Widget B', 'price' => 15],
     ['name' => 'Widget C', 'price' => 20],
 ];
 
-// Calculate and display total price
-$totalPrice = calculateTotalPrice($items);
-echo "Total price: $" . $totalPrice;
+$total = calculateTotalPrice($items);
+echo "Total price: $" . $total;
+function modifyString($string)
+{
+    $string = str_replace(' ', '', $string);
+    $string = strtolower($string);
 
-// String manipulation
-$inputString = "This is a poorly written program with little structure and readability.";
-$modifiedString = modifyString($inputString);
+    return $string;
+}
+$string = "This is a poorly written program with little
+structure and readability.";
+$modifiedString = modifyString($string);
 echo "\nModified string: " . $modifiedString;
-
-// Check if a number is even or odd
-$number = 42;
-$result = checkEvenOrOdd($number);
-echo "<div\n$result";
-?>
+function checkEvenOrOdd($number)
+{
+    if ($number % 2 == 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
+$number = 51;
+$numberType = checkEvenOrOdd($number);
+echo "\nThe number " . $number . " is " . $numberType . ".";
