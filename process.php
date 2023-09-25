cfe4a3f1171f7681924dfc954f3d44870e91de5e
KrazyyyPotatoo <116055802+KrazyyyPotatoo@users.noreply.github.com>
2023-09-25 21:05:05 +0800

    word-frequency-counter

--- process.php
+++ process.php
@@ -1,86 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Word Frequency Results</title>
-    <style>
-        /* Your CSS styles here. */
-    </style>
-</head>
-<body>
-    <h1>Word Frequency Results</h1>
-
-    <?php
-    // Function to calculate word frequency while ignoring stop words.
-    function calculateWordFrequency($words, $stopWords) {
-        $wordFrequency = array_count_values($words);
-
-        // Remove stop words from the frequency array
-        foreach ($stopWords as $stopWord) {
-            unset($wordFrequency[$stopWord]);
-        }
-
-        return $wordFrequency;
-    }
-
-    // Function to sort word frequency based on user's choice.
-    function sortWordFrequency($wordFrequency, $sortOrder) {
-        if ($sortOrder === "asc") {
-            asort($wordFrequency);
+<?php
+   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $text = $_POST['text'];
+    $sort= $_POST['sort'];
+    $limit = $_POST['limit'];
+   }
+    function calculate_Word_Frequency($text, $sort, $limit) {
+        $words = str_word_count($text, 1);
+        $stopWords = ["the", "and", "in"];
+        $wordCounts = array_count_values(array_diff($words, $stopWords));
+        if ($sort === "asc") {
+            asort($wordCounts);
         } else {
-            arsort($wordFrequency);
+            arsort($wordCounts);
         }
-        return $wordFrequency;
+        $wordCounts = array_slice($wordCounts, 0, $limit);
+        return $wordCounts;
     }
-
-    // Function to limit the number of words displayed
-    function limitWordFrequency($wordFrequency, $limit) {
-        return array_slice($wordFrequency, 0, $limit);
-    }
-
-    // Define stop words
-    $stopWords = ["the", "and", "in"]; // Common stop words to ignore
-
-    // Check if the form is submitted
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Retrieve user input and perform input validation
-        $inputText = trim($_POST['text']);
-        $selectedSortOrder = $_POST['sort']; // 'asc' or 'desc'
-        $selectedLimit = (int)$_POST['limit']; // Number of words to display
-
-        // Validate input
-        if (empty($inputText) || $selectedLimit <= 0) {
-            echo '<p>Please enter valid input and limit.</p>';
-        } else {
-            // Tokenize the input text into words
-            $words = str_word_count($inputText, 1);
-
-            // Calculate word frequency
-            $wordFrequency = calculateWordFrequency($words, $stopWords);
-
-            // Sort word frequency based on user's choice
-            $sortedWordFrequency = sortWordFrequency($wordFrequency, $selectedSortOrder);
-
-            // Limit the number of words to display
-            $limitedWordFrequency = limitWordFrequency($sortedWordFrequency, $selectedLimit);
-
-            // Output the results in a styled table
-            echo '<table>';
-            echo '<tr><th>Word</th><th>Frequency</th></tr>';
-
-            foreach ($limitedWordFrequency as $word => $frequency) {
-                // HTML escape user-generated content
-                $word = htmlspecialchars($word);
-                echo "<tr><td>$word</td><td>$frequency</td></tr>";
+        if (!empty($text)) {
+            $wordFrequencies = calculate_Word_Frequency($text, $sort, $limit);
+            echo "<h2>Word Frequency:</h2>";
+            echo "<ul>";
+            foreach ($wordFrequencies as $word => $frequency) {
+                echo "<li>$word: $frequency</li>";
             }
-
-            echo '</table>';
+            echo "</ul>";
+        } else {
+            echo "<p>Please enter some text.</p>";
         }
-    } else {
-        // Handle the case where the form is not submitted
-        echo '<p>No data submitted.</p>';
-    }
-    ?>
-
-</body>
-</html>
+?>
